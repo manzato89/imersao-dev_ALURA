cf8fbdc955e9599f39268e434d4951f3ce3af5e7
Anderson Ricardo Manzato <manzato89@gmail.com>
2021-09-16 15:00:50 -0300

    Aula 1 - Variaveis - Operacoes - Media

---
+++ calculadora-de-m-dia/README.markdown
@@ -0,0 +1,5 @@
+# Calculadora de média
+
+A Pen created on CodePen.io. Original URL: [https://codepen.io/manzato89/pen/LYLzyVJ](https://codepen.io/manzato89/pen/LYLzyVJ).
+
+
---
+++ calculadora-de-m-dia/dist/script.js
@@ -0,0 +1,15 @@
+var nome = "Anderson"
+var notaDoPrimeiroBimestre = 9.6995
+var notaDoSegundoBimestre = 7.8913
+var notaDoTerceiroBimestre = 4.5654
+var notaDoQuartoBimestre = 2.5455
+
+var notaFinal = (notaDoPrimeiroBimestre + notaDoSegundoBimestre + notaDoTerceiroBimestre + notaDoQuartoBimestre) / 4
+
+var notafixada = notaFinal.toFixed(1)
+
+console.log("Bem vindo " + nome)
+console.log(notafixada)
+
+//Revisão
+//Variáveis, strings, console.log, ToFixed, operações matemáticas, concatenação.
